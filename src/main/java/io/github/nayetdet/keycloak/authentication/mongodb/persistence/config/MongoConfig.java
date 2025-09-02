91128f73a72012a2c566990ee5382b2b165668dd
nayetdet <jpmoreiracampos21@gmail.com>
2025-09-02 00:04:43 -0300

    feat: finalizado desenvolvimento

--- src/main/java/io/github/nayetdet/keycloak/authentication/mongodb/persistence/config/MongoConfig.java
+++ src/main/java/io/github/nayetdet/keycloak/authentication/mongodb/persistence/config/MongoConfig.java
@@ -1,9 +1,11 @@
 package io.github.nayetdet.keycloak.authentication.mongodb.persistence.config;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import org.bson.UuidRepresentation;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import org.eclipse.microprofile.config.ConfigProvider;
 
@@ -20,7 +22,13 @@ public class MongoConfig {
 
     public static synchronized MongoClient getMongoClient() {
         if (mongoClient == null) {
-            mongoClient = MongoClients.create(ConfigProvider.getConfig().getValue("quarkus.mongodb.connection-string", String.class));
+            var settings = MongoClientSettings
+                    .builder()
+                    .applyConnectionString(new ConnectionString(ConfigProvider.getConfig().getValue("quarkus.mongodb.connection-string", String.class)))
+                    .uuidRepresentation(UuidRepresentation.STANDARD)
+                    .build();
+
+            mongoClient = MongoClients.create(settings);
         }
 
         return mongoClient;
